f5de2fddea585db5106f0504432b0d9a20277007
ammieprsd <ammieprsd@gmail.com>
2022-04-26 00:58:09 +0200

    added getters and setters in CustomerController

--- CustomerController.java
+++ CustomerController.java
@@ -11,4 +11,39 @@ public class CustomerController {
         this.age = age;
         this.address = address;
     }
+
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
