da059700b1c5f557574c3f17650fc63f4536301b
ammieprsd <ammieprsd@gmail.com>
2022-04-26 11:58:16 +0200

    customerController

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        CustomerController customerController = new CustomerController("Martijn", "De Vries", 65, "The Hague");
+        customerController.setFirstName("Ammie");
+        System.out.println(customerController.getFirstName());
+
     }
 }
