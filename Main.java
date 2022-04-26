cd9cd54c6aa89fc7ab93763091796926a896495a
ammieprsd <ammieprsd@gmail.com>
2022-04-26 12:04:42 +0200

    naam verandert

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         CustomerController customerController = new CustomerController("Martijn", "De Vries", 65, "The Hague");
-        customerController.setFirstName("Ammie");
+        customerController.setFirstName("Martijn");
         System.out.println(customerController.getFirstName());
 
     }
